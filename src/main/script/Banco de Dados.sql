1670a03d2a853ddce038b183574af8bd4df4161d
Tommye <tommyesilva@totemti.com.br>
2016-11-14 10:49:52 -0300

    Script de criação para o MySql Workbeanch - Só executar a script que irá adicionar o básico e também criará o banco de dados.

--- src/main/script/Banco de Dados.sql
+++ src/main/script/Banco de Dados.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `erp` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `erp`;
--- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: erp
 -- ------------------------------------------------------
--- Server version	5.7.12-log
+-- Server version	5.7.14-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,8 +26,8 @@ DROP TABLE IF EXISTS `perfil`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `perfil` (
   `IDPERFIL` bigint(20) NOT NULL AUTO_INCREMENT,
-  `NOME` varchar(80) NOT NULL,
-  `SITUACAO` varchar(50) NOT NULL,
+  `NOME` varchar(80) DEFAULT NULL,
+  `SITUACAO` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`IDPERFIL`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -38,7 +38,7 @@ CREATE TABLE `perfil` (
 
 LOCK TABLES `perfil` WRITE;
 /*!40000 ALTER TABLE `perfil` DISABLE KEYS */;
-INSERT INTO `perfil` VALUES (1,'ADMINISTRADOR','ATIVO'),(2,'USUARIO','ATIVO');
+INSERT INTO `perfil` VALUES (1,'Administrador','ATIVO'),(2,'Tester','INATIVO');
 /*!40000 ALTER TABLE `perfil` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,19 +51,18 @@ DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
   `IDUSUARIO` bigint(20) NOT NULL AUTO_INCREMENT,
-  `CPF` varchar(15) NOT NULL,
-  `EMAIL` varchar(40) NOT NULL,
-  `LOGIN` varchar(20) NOT NULL,
-  `NOME` varchar(80) NOT NULL,
-  `PARAMETROPAGINACAO` int(11) DEFAULT NULL,
-  `SENHA` varchar(50) NOT NULL,
-  `SITUACAO` varchar(50) NOT NULL,
+  `CPF` varchar(15) DEFAULT NULL,
+  `EMAIL` varchar(50) DEFAULT NULL,
+  `LOGIN` varchar(20) DEFAULT NULL,
+  `NOME` varchar(80) DEFAULT NULL,
+  `SENHA` varchar(50) DEFAULT NULL,
+  `SITUACAO` varchar(255) NOT NULL,
   `PERFIL_ID` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`IDUSUARIO`),
   UNIQUE KEY `LOGIN` (`LOGIN`),
   KEY `FK22E07F0EEDE82E8E` (`PERFIL_ID`),
   CONSTRAINT `FK22E07F0EEDE82E8E` FOREIGN KEY (`PERFIL_ID`) REFERENCES `perfil` (`IDPERFIL`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,9 +71,13 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'69410291100','tommy_vinicius@hotmail.com','root','Tommye Vinícius Toledo Silva',20,'b4b8daf4b8ea9d39568719e1e320076f','ATIVO',1);
+INSERT INTO `usuario` VALUES (1,'69410291100','tommy_vinicius@hotmail.com','root','Tommye Vinícius','b4b8daf4b8ea9d39568719e1e320076f','ATIVO',1),(2,'14612314497','samuel@totemti.com.br','samu','Samuel Figueiredo','a7023330e5297446be99a86787434e4f','ATIVO',1);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'erp'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -85,4 +88,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-12 19:05:15
+-- Dump completed on 2016-11-14 10:46:03
